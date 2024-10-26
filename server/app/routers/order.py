ef99490cc487ca80a430bf9abb51cdb76523da54
情野陸 <rikuseino33@gmail.com>
2024-10-26 22:25:58 +0900

    feat: orderを追加するエンドポイントを実装

--- server/app/routers/order.py
+++ server/app/routers/order.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Path, Depends
 from app.cruds import customer_auth as customer_auth_cruds
+from app.cruds import order as order_cruds
 from starlette import status
-from app.schemas import DecodedToken, OrderCreate
+from app.schemas import DecodedToken, OrderCreate, OrderResponse
 from sqlalchemy.orm import Session
 from app.database import get_db
 from typing import Annotated
@@ -12,6 +13,6 @@ CustomerDependency = Annotated[DecodedToken, Depends(customer_auth_cruds.get_cur
 
 router = APIRouter(prefix="/customer/order", tags=["auth"])
 
-@router.post("/{r_id}", status_code=status.HTTP_201_CREATED)
+@router.post("/{r_id}", response_model=OrderResponse, status_code=status.HTTP_201_CREATED)
 async def create_order(db: DbDependency, customer: CustomerDependency, order_create: OrderCreate, r_id: int=Path(gt=0)):
-    return customer_auth_cruds.create_order(db, order_create, customer.id, r_id)
+    return order_cruds.create(db, order_create, r_id, customer.id)
