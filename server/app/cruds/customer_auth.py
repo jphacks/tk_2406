6314345442043170ac626734642a8bbe1bf313fa
keltX <nigelwijaya7@gmail.com>
2024-10-26 15:50:21 +0900

    customer_auth fix

--- server/app/cruds/customer_auth.py
+++ server/app/cruds/customer_auth.py
@@ -17,13 +17,14 @@ SECRET_KEY = get_settings().secret_key
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def create_customer(db: Session, customer_create: CustomerCreate):
-    existing_user = db.query(Customer).filter(Customer.c_name == customer_create.r_name).first()
+    existing_user = db.query(Customer).filter(Customer.c_name == customer_create.c_name).first()
     if existing_user:
         return None
     salt = base64.b64encode(os.urandom(32))
     hashed_password = hashlib.pbkdf2_hmac("sha256", customer_create.password.encode(), salt, 10).hex()
     new_customer = Customer(
         c_name=customer_create.c_name,
+        email=customer_create.email,
         password=hashed_password,
         salt=salt.decode()
     )
@@ -31,8 +32,8 @@ def create_customer(db: Session, customer_create: CustomerCreate):
     db.commit()
     return new_customer
 
-def authenticate_customer(db: Session, r_name: str, password: str):
-    customer = db.query(Customer).filter(Customer.r_name == r_name).first()
+def authenticate_customer(db: Session, c_name: str, password: str):
+    customer = db.query(Customer).filter(Customer.c_name == c_name).first()
     if not customer:
         return None
     hashed_password = hashlib.pbkdf2_hmac("sha256", password.encode(), customer.salt.encode(), 10).hex()
@@ -40,19 +41,19 @@ def authenticate_customer(db: Session, r_name: str, password: str):
         return None
     return customer
 
-def create_access_token(r_name: str, r_id: int, expires_delta: timedelta):
+def create_access_token(c_name: str, c_id: int, expires_delta: timedelta):
     expires = datetime.now() + expires_delta
-    payload = {"sub": r_name, "id": r_id, "exp": expires}
+    payload = {"sub": c_name, "id": c_id, "exp": expires}
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
 def get_current_customer(token: Annotated[str, Depends(oauth2_schema)]):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        r_name = payload.get("sub")
-        r_id = payload.get("id")
-        if r_name is None or r_id is None:
+        c_name = payload.get("name")
+        c_id = payload.get("id")
+        if c_name is None or c_id is None:
             return None
-        return DecodedToken(name=r_name, id=r_id)
+        return DecodedToken(name=c_name, id=c_id)
 
     except JWTError:
         raise JWTError
