220606c337266c9a91688ecc8ce7a7100ef23d91
Mattya3 <m.tkm1217@ezweb.ne.jp>
2024-10-26 17:54:00 +0900

    modify server

--- server/app/models.py
+++ server/app/models.py
@@ -3,9 +3,9 @@ from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, Foreig
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
+
 class Restaurant(Base):
     __tablename__ = "restaurants"
-    __table_args__ = {'extend_existing': True}
     r_id = Column(Integer, primary_key=True)
     r_name = Column(String(255), nullable=False, unique=True)
     password = Column(String(255), nullable=False)
@@ -21,20 +21,12 @@ class Food(Base):
     t_id = Column(Integer, ForeignKey("tags.t_id", ondelete="CASCADE"), nullable=False)
     is_alcohol = Column(Boolean, nullable=False)
     restaurant = relationship("Restaurant", back_populates="foods")
+    tag = relationship("Tag", back_populates="foods")
 
 Restaurant.foods = relationship("Food", order_by=Food.f_id, back_populates="restaurant")
 
-class Tag(Base):
-    __tablename__ = "tags"
-    __table_args__ = {'extend_existing': True}
-    t_id = Column(Integer, primary_key=True)
-    t_name = Column(String(255), nullable=False, unique=True)
-    
-Restaurant.tags = relationship("Tag", order_by=Tag.t_id, back_populates="restaurant")
-
 class FoodAlcohol(Base):
     __tablename__ = "food_alcohol"
-    __table_args__ = {'extend_existing': True}
     f_id = Column(Integer, ForeignKey("foods.f_id", ondelete="CASCADE"), primary_key=True)
     degree = Column(Float, nullable=False)
     f_quantity = Column(Integer, nullable=False)
@@ -43,9 +35,18 @@ class FoodAlcohol(Base):
 
 Food.alcohol = relationship("FoodAlcohol", uselist=False, back_populates="food")
 
+class Tag(Base):
+    __tablename__ = "tags"
+    __table_args__ = {'extend_existing': True}
+    t_id = Column(Integer, primary_key=True)
+    r_id = Column(Integer, ForeignKey("restaurants.r_id", ondelete="CASCADE"), nullable=False)
+    t_name = Column(String(255), nullable=False, unique=True)
+    food = relationship("Food", back_populates="tag") 
+
+Food.tag = relationship("Tag", uselist=False, back_populates="food")
+
 class Customer(Base):
     __tablename__ = "customers"
-    __table_args__ = {'extend_existing': True}
     c_id = Column(Integer, primary_key=True)
     c_name = Column(String(255), nullable=False, unique=True)
     email = Column(String(255), nullable=False)
@@ -54,7 +55,6 @@ class Customer(Base):
 
 class Order(Base):
     __tablename__ = "orders"
-    __table_args__ = {'extend_existing': True}
     o_id = Column(Integer, primary_key=True)
     c_id = Column(Integer, ForeignKey("customers.c_id", ondelete="CASCADE"), nullable=False)
     r_id = Column(Integer, ForeignKey("restaurants.r_id", ondelete="CASCADE"), nullable=False)
@@ -68,7 +68,6 @@ Restaurant.orders = relationship("Order", order_by=Order.o_id, back_populates="r
 
 class OrderItem(Base):
     __tablename__ = "order_items"
-    __table_args__ = {'extend_existing': True}
     oi_id = Column(Integer, primary_key=True)
     o_id = Column(Integer, ForeignKey("orders.o_id", ondelete="CASCADE"), nullable=False)
     f_id = Column(Integer, ForeignKey("foods.f_id", ondelete="CASCADE"), nullable=False)
@@ -81,7 +80,6 @@ Order.items = relationship("OrderItem", order_by=OrderItem.oi_id, back_populates
 
 class Evaluation(Base):
     __tablename__ = "evaluations"
-    __table_args__ = {'extend_existing': True}
     e_id = Column(Integer, primary_key=True)
     c_id = Column(Integer, ForeignKey("customers.c_id", ondelete="CASCADE"), nullable=False)
     e_day = Column(DateTime, default=datetime.now)
