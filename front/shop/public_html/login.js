f3d13c6e645ac1563bf062240ce545cdb62a8ea0
Tooooooooooooo <jj_blue207@yahoo.co.jp>
2024-10-26 17:04:47 +0900

    ログイン・サインアップコメント追加

--- front/shop/public_html/login.js
+++ front/shop/public_html/login.js
@@ -1,21 +1,30 @@
 
 config = Config()
 
+//エラーメッセージ表示
 function setMessage(str, color = "red") {
     document.getElementById("message").innerHTML = "<div style='color:" + color + ";'>" + str + "</div>"
 }
 
+//遅延つき画面遷移
 function moveSignup() {
     document.getElementById("body").innerHTML = ""
     document.getElementById("body").style = "background-color: #000000;"
     setTimeout(function () { window.location.href = "signup.html" }, 500)
 }
+
+//ログイン
 function login() {
+    //cookie削除
     document.cookie = "access_token=; path=/; max-age=86400;"
     document.cookie = "token_type=; path=/; max-age=86400;"
+
+    //入力値取得
     const id = document.getElementById("user").value
     const pw = document.getElementById("pass").value
     const button = document.getElementById("login_btn")
+
+    //入力値チェック
     if (id === "" && pw === "") {
         setMessage("ユーザー名とパスワードを入力してください")
         return
@@ -24,6 +33,7 @@ function login() {
         return
     }
 
+    //ローディング
     button.disabled = true
 
     const xhr = new XMLHttpRequest()
@@ -31,11 +41,13 @@ function login() {
     xhr.onload = function () {
         if (xhr.readyState == 4) {
             if (xhr.status == 200) {
+                //ログイン成功時
                 const data = JSON.parse(xhr.responseText)
                 document.cookie = `access_token=${data.access_token}; path=/; max-age=86400;`
                 document.cookie = `token_type=${data.token_type}; path=/; max-age=86400;`
                 window.location.href = "item.html"
             } else {
+                //ログイン失敗時
                 setMessage('ログインに失敗しました。店舗ネームまたはパスワードを確認してください。');
                 button.disabled = false
             }
@@ -43,6 +55,7 @@ function login() {
     }
 
     xhr.onerror = () => {
+        //通信失敗時
         alert(xhr.readyState)
         alert(xhr.status)
         setMessage('サーバーに接続できません。ネットワーク環境をご確認ください。');
@@ -50,10 +63,12 @@ function login() {
 
     };
 
+    //POST送信
     xhr.open("POST", `${config.server_protocol}://${config.server_ip}:${config.server_port}/restaurant/login`)
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
     xhr.send("username=" + id + "&password=" + pw)
 
+    //表示
     document.getElementById("pass").value = ""
     setMessage("ログイン中...", "black")
 
--- front/shop/public_html/signup.js
+++ front/shop/public_html/signup.js
@@ -1,21 +1,29 @@
 
 config=Config()
 
+//エラーメッセージ表示
 function setMessage(str,color="red") {
     document.getElementById("message").innerHTML = "<div style='color:"+color+";'>" + str + "</div>"
 }
+//遅延つき画面遷移
 function moveLogin() {
     document.getElementById("body").innerHTML=""
     document.getElementById("body").style="background-color: #000000;"
     setTimeout(function(){window.location.href = "/"}, 500)
 }
 
+
 function login() {
+    //cookie削除
     document.cookie = "access_token=; path=/; max-age=86400;"
     document.cookie = "token_type=; path=/; max-age=86400;"
+
+    //入力値取得
     const id = document.getElementById("user").value
     const pw = document.getElementById("pass").value
     const button = document.getElementById("login_btn")
+
+    //入力値チェック
     if (id === "" && pw === "") {
         setMessage("ユーザー名とパスワードを入力してください")
         return
@@ -24,6 +32,7 @@ function login() {
         return
     }
 
+    //ローディング
     button.disabled=true
 
     const xhr = new XMLHttpRequest()
@@ -31,11 +40,13 @@ function login() {
     xhr.onload = function () {
         if (xhr.readyState == 4) {
             if (xhr.status == 201) {
+                //登録成功時
                 const data = JSON.parse(xhr.responseText)
                 document.cookie = `access_token=${data.access_token}; path=/; max-age=86400;`
                 document.cookie = `token_type=${data.token_type}; path=/; max-age=86400;`
                 window.location.href = "item.html"
             } else {
+                //登録失敗時
                 setMessage('この店ネームは既に使用されているため登録できません。');
                 button.disabled=false
             }
@@ -43,15 +54,18 @@ function login() {
     }
 
     xhr.onerror = () => {
+        //通信失敗時
         setMessage('サーバーに接続できません。ネットワーク環境をご確認ください。');
         button.disabled=false
 
     };
 
+    //送信
     xhr.open("POST", `${config.server_protocol}://${config.server_ip}:${config.server_port}/restaurant/signup`)
     xhr.setRequestHeader("Content-Type", "application/json")
     xhr.send(`{\"r_name\":\"${id}\",\"password\":\"${pw}\"}`)
 
+    //入力値クリア
     document.getElementById("pass").value = ""
     setMessage("登録中...", "black")
 
