85cd7f8fb9156359c83bb03389fe49a8d891f58d
Mattya3 <m.tkm1217@ezweb.ne.jp>
2024-10-26 11:04:36 +0900

    uplode templete-server

---
+++ server/models.py
@@ -0,0 +1,29 @@
+from server.database import Base
+from sqlalchemy import Column, Integer, String, Enum, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from server.schemas import ItemStatus
+from datetime import datetime
+
+# class Item(Base):
+#     __tablename__ = "items"
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String, nullable=False)
+#     price = Column(Integer, nullable=False)
+#     description = Column(String, nullable=True)
+#     status = Column(Enum(ItemStatus), nullable=False, default=ItemStatus.ON_SALE)
+#     created_at = Column(DateTime, default=datetime.now())
+#     updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
+#     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+#     user = relationship("User", back_populates="items")
+
+# class User(Base):
+#     __tablename__ = "users"
+#     id = Column(Integer, primary_key=True)
+#     username = Column(String, nullable=False, unique=True)
+#     password = Column(String, nullable=False)
+#     salt = Column(String, nullable=False)
+#     created_at = Column(DateTime, default=datetime.now())
+#     updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
+
+#     items = relationship("Item", back_populates="user")
