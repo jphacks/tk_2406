f2004393057d8f1e074b50d36207dfe2df0ef69c
keltX <nigelwijaya7@gmail.com>
2024-10-27 10:56:59 +0900

    dish_create fix

--- server/app/routers/restaurant.py
+++ server/app/routers/restaurant.py
@@ -15,7 +15,7 @@ FormDependency = Annotated[OAuth2PasswordRequestForm, Depends()]
 router = APIRouter(prefix="/restaurant", tags=["restaurants"])
 
 @router.post("/signup", response_model=Token, status_code=status.HTTP_201_CREATED)
-async def create_restaurant(db: DbDependency, restaurant_create: RestaurantCreate):
+async def create_restaurant(db: DbDependency, restaurant_create: RestaurantCreate): # type: ignore
     restaurant = restaurant_auth_cruds.create_restaurant(db, restaurant_create)
     if not restaurant:
         raise HTTPException(status_code=409, detail="Restaurant already has")
@@ -23,15 +23,15 @@ async def create_restaurant(db: DbDependency, restaurant_create: RestaurantCreat
     return {"access_token": token, "token_type": "bearer"}
 
 @router.post("/login", response_model=Token, status_code=status.HTTP_200_OK)
-async def login(db: DbDependency, form_data: FormDependency):
+async def login(db: DbDependency, form_data: FormDependency): # type: ignore
     restaurant = restaurant_auth_cruds.authenticate_restaurant(db, form_data.username, form_data.password)
     if not restaurant:
         raise HTTPException(status_code=401, detail="Incorrect rname or password")
     token = restaurant_auth_cruds.create_access_token(restaurant.r_name, restaurant.r_id, timedelta(minutes=240))
     return {"access_token": token, "token_type": "bearer"}
 
-@router.post("/tag",dependencies=[Depends(JWTBearer())], status_code=status.HTTP_201_CREATED)
-async def create_tag(db: DbDependency, tag_create: TagCreate, token:str = Depends(JWTBearer())):
+@router.post("/tag", status_code=status.HTTP_201_CREATED)
+async def create_tag(db: DbDependency, tag_create: TagCreate, token:str = Depends(JWTBearer())): # type: ignore
     r_name, r_id = restaurant_auth_cruds.decode_token(token)
     if not r_id:
         raise HTTPException(status_code=401, detail="Invalid token")
@@ -40,8 +40,8 @@ async def create_tag(db: DbDependency, tag_create: TagCreate, token:str = Depend
         raise HTTPException(status_code=409, detail="Tag already exists")
     return tag
     
-@router.get("/dish",dependencies=[Depends(JWTBearer())],response_model=List[DishResponse], status_code=status.HTTP_200_OK)
-async def get_dish(db:DbDependency, t_id: Optional[int] = Query(None, gt=0, examples=[1]), token:str = Depends(JWTBearer())):
+@router.get("/dish",response_model=List[DishResponse], status_code=status.HTTP_200_OK)
+async def get_dish(db:DbDependency, t_id: Optional[int] = Query(None, gt=0, examples=[1]), token:str = Depends(JWTBearer())): # type: ignore
     r_name, r_id = restaurant_auth_cruds.decode_token(token)
     if t_id:
         dishes = menu_cruds.get_dishes_by_tag(db, r_id[1], t_id)
@@ -49,28 +49,26 @@ async def get_dish(db:DbDependency, t_id: Optional[int] = Query(None, gt=0, exam
         dishes = menu_cruds.get_dish_all(db, r_id[1])
     return dishes
 
-@router.post("/dish",dependencies=[Depends(JWTBearer())], response_model=DishResponse, status_code=status.HTTP_201_CREATED)
-async def create_dish(db: DbDependency, dish_create: DishCreate, token:str = Depends(JWTBearer())):
+@router.post("/dish", response_model=DishResponse, status_code=status.HTTP_201_CREATED)
+async def create_dish(db: DbDependency, dish_create: DishCreate, token:str = Depends(JWTBearer())): # type: ignore
     r_name, r_id = restaurant_auth_cruds.decode_token(token)
-    if not r_id:
-        raise HTTPException(status_code=401, detail="Invalid token")
-    f_name, price, t_id, is_alcohol,degree = dish_create.f_name, dish_create.price, dish_create.t_id, dish_create.is_alcohol, dish_create.degree
-    new_dish = menu_cruds.create_dish(db, r_id[1], f_name, price, t_id, is_alcohol, degree)
+    f_name, price, t_id, is_alcohol,degree, f_quantity = dish_create.f_name, dish_create.price, dish_create.t_id, dish_create.is_alcohol, dish_create.degree, dish_create.f_quantity
+    new_dish = menu_cruds.create_dish(db, r_id[1], f_name, price, t_id, is_alcohol, degree, f_quantity)
 
     if not new_dish:
         raise HTTPException(status_code=409, detail="Dish already exists")
     
     return new_dish
 
-@router.put("/dish/{f_id}", dependencies=[Depends(JWTBearer())], response_model=DishResponse, status_code=status.HTTP_202_ACCEPTED)
-async def update_dish(db: DbDependency, f_id: int, dish_update: DishUpdate, token:str = Depends(JWTBearer())):
+@router.put("/dish/{f_id}",  response_model=DishResponse, status_code=status.HTTP_202_ACCEPTED)
+async def update_dish(db: DbDependency, f_id: int, dish_update: DishUpdate, token:str = Depends(JWTBearer())): # type: ignore
     r_name, r_id = restaurant_auth_cruds.decode_token(token)
     if not r_id:
         raise HTTPException(status_code=401, detail="Invalid token")
     
     # Filter out None values from the update dictionary
     update_data = {k: v for k, v in dish_update.__dict__.items() if v is not None}
-    
+
     # Update the dish with only the provided fields
     updated_dish = menu_cruds.update_dish(db, f_id, r_id[1], updated_items=update_data)
     if updated_dish == -1:
