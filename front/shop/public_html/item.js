7eb4c56bd73b7c794bcc68454ae05c475080e2d4
Tooooooooooooo <jj_blue207@yahoo.co.jp>
2024-10-27 11:35:43 +0900

    店認証

--- front/shop/public_html/item.js
+++ front/shop/public_html/item.js
@@ -1,9 +1,9 @@
 config = Config()
 
-let tags=[]
-let tag_sel=-1
-let foods=[]
-let food_sel=-1
+let tags = []
+let tag_sel = -1
+let foods = []
+let food_sel = -1
 let addbtn_edit = false
 
 
@@ -14,10 +14,13 @@ const cookies = document.cookie;
 const array = cookies.split('; ');
 array.forEach(function (value) {
     const content = value.split('=');
-    cookie_dict[content[0]] = content[1];
+    let nval = content[1]
+    if (nval == "bearer") nval = "Bearer";
+    cookie_dict[content[0]] = nval;
 })
 
 if (cookie_dict["access_token"] === undefined || cookie_dict["token_type"] === undefined) {
+    alert("セッションが期限切れです。")
     window.location.href = "/"
 }
 
@@ -28,39 +31,39 @@ const ele_tags = document.getElementById("tags")
 const ele_addbtn = document.getElementById("add_btn")
 
 //仮
-for(let i=0;i<100;i++){
-    foods.push({f_name:i+"ねnameあああああああああああああああああああああああああああああああああああああああああああああああああああああ"+(i+1),price:100*(i+1)})
+for (let i = 0; i < 100; i++) {
+    foods.push({ f_name: i + "ねnameあああああああああああああああああああああああああああああああああああああああああああああああああああああ" + (i + 1), price: 100 * (i + 1) })
 }
 setFood()
-tags=["A","BB","CCC","DDDD","BB","CCC","DDDD","BB","CCC","DDDD","BB","CCC","DDDD"]
+tags = ["A", "BB", "CCC", "DDDD", "BB", "CCC", "DDDD", "BB", "CCC", "DDDD", "BB", "CCC", "DDDD"]
 setTags()
 
 getAllTags()
 getAllItems()
 
-function del(id){
-    const res=window.confirm(""+id+": 削除しますか?")
+function del(id) {
+    const res = window.confirm("" + id + ": 削除しますか?")
     alert(res)
 }
 
 
-function edit(id){
-    if(food_sel>=0) document.getElementById("food"+food_sel).style.backgroundColor=""
-    food_sel=Number(id)
+function edit(id) {
+    if (food_sel >= 0) document.getElementById("food" + food_sel).style.backgroundColor = ""
+    food_sel = Number(id)
     ele_addbtn.innerHTML = "更新"
     addbtn_edit = true
-    document.getElementById("food"+food_sel).style.backgroundColor="#ff0000"
+    document.getElementById("food" + food_sel).style.backgroundColor = "#ff0000"
 }
 
-function setTags(){
+function setTags() {
 
     let html = ""
 
-    let i=0
+    let i = 0
     for (const tag of tags) {
-        let background=""
-        if(i==tag_sel){
-            background="background-color:#ff0000;"
+        let background = ""
+        if (i == tag_sel) {
+            background = "background-color:#ff0000;"
         }
 
         html += `
@@ -92,28 +95,28 @@ function setTags(){
 
 }
 
-function getAllTags(){
+function getAllTags() {
 
 }
 
 function setFood() {
     let html = ""
 
-    let i=0
+    let i = 0
     for (const food of foods) {
-        let background=""
+        let background = ""
 
-        if(i==food_sel){
-            background="background-color:#ff0000;"
+        if (i == food_sel) {
+            background = "background-color:#ff0000;"
         }
 
-        let pos="49.5%"
-        if(i%2==0){
-            html+=`<div style="position: relative">`
-            pos="0%"
+        let pos = "49.5%"
+        if (i % 2 == 0) {
+            html += `<div style="position: relative">`
+            pos = "0%"
         }
 
-        const top=""+(Math.floor(i/2)*14)+"vh"
+        const top = "" + (Math.floor(i / 2) * 14) + "vh"
 
         html += `
         <div class="box2" id="food${i}" style="left:${pos};top:${top};position:absolute;${background}">
@@ -168,17 +171,17 @@ function setFood() {
         </div>
         `
 
-        if(i%2==1){
-            html+="</div>"
+        if (i % 2 == 1) {
+            html += "</div>"
         }
         i++
     }
-    if(i%2==1){
-        html+="</div>"
+    if (i % 2 == 1) {
+        html += "</div>"
     }
-    const top2=""+(Math.floor(1+(i-1)/2)*14)+"vh"
+    const top2 = "" + (Math.floor(1 + (i - 1) / 2) * 14) + "vh"
 
-    html+=`<div style='height: 5vh;top:${top2};position:absolute;'>　</div>`
+    html += `<div style='height: 5vh;top:${top2};position:absolute;'>　</div>`
 
 
 
@@ -193,38 +196,40 @@ function getAllItems(tag) {
                 const data = JSON.parse(xhr.responseText)
                 console.log(data)
             } else {
-                alert(""+xhr.status+"\n"+xhr.responseText)
+                alert("セッションが期限切れです。")
+                window.location.href = "/"
             }
         }
     }
 
     xhr.onerror = () => {
-        ele_foods.innerHTML = "<div style='color:red;font-size: 5vh;'>サーバーに接続できません。ネットワーク環境をご確認ください。</div>"
+        alert("通信に失敗しました。")
+        window.location.href = "/"
     }
 
     xhr.open("GET", `${config.server_protocol}://${config.server_ip}:${config.server_port}/restaurant/dish`)
     xhr.setRequestHeader("Content-Type", "application/json")
-    xhr.setRequestHeader("access_token", cookie_dict["access_token"])
-    xhr.setRequestHeader("token_type", cookie_dict["token_type"])
+    const auth = (cookie_dict["token_type"] + " " + cookie_dict["access_token"])
+    xhr.setRequestHeader("Authorization", auth)
     xhr.send()
 }
 
 function clickTag(tag) {
-    if(tag_sel>=0) document.getElementById("tag"+tag_sel).style.backgroundColor=""
-    tag_sel=Number(tag)
-    food_sel=-1
-    document.getElementById("tag"+tag_sel).style.backgroundColor="#ff0000"
+    if (tag_sel >= 0) document.getElementById("tag" + tag_sel).style.backgroundColor = ""
+    tag_sel = Number(tag)
+    food_sel = -1
+    document.getElementById("tag" + tag_sel).style.backgroundColor = "#ff0000"
     setFood()
 }
 
 function newItem() {
-    if(food_sel>=0) document.getElementById("food"+food_sel).style.backgroundColor=""
+    if (food_sel >= 0) document.getElementById("food" + food_sel).style.backgroundColor = ""
     addbtn_edit = false
     ele_addbtn.innerHTML = "新規追加"
 }
 
 function qr() {
-    window.location.href = "qr.html"
+    window.open("qr.html")
 }
 
 function setMessage(str, color = "red") {
